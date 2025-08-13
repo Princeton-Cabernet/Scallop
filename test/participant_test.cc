0894c3e0a73e84967e4c1900175422377145d88e
Oliver Michel <omichel@princeton.edu>
2025-08-13 18:07:27 -0500

    remove more code, ignore .vscode

--- test/participant_test.cc
+++ test/participant_test.cc
@@ -26,39 +26,3 @@ TEST_CASE("Participant: addOutgoingStream", "[participant]") {
     CHECK(stream.description().msid.has_value());
     CHECK(stream.description().msid->appData == "42f41fd3-296c-468a-8bd6-b8c4bf60daa9");
 }
-/*
-TEST_CASE("Participant: sendsStreamWithMid", "[participant]") {
-
-    Session s{0, test::sfuConfig};
-    auto id = s.addParticipant("127.0.0.1", 23292);
-    auto participant = s.getParticipant(id);
-
-    SECTION("returns false for a non-existing stream") {
-        CHECK_FALSE(participant.sendsStreamWithMid("0"));
-    }
-
-    SECTION("returns true for an existing stream") {
-        SDP::SessionDescription sdp(test::sdpOfferLines1);
-        participant.addOutgoingStream(Stream{sdp.mediaDescriptions[0]});
-        CHECK(participant.sendsStreamWithMid("0"));
-    }
-}
-*/
-/*
-TEST_CASE("Participant: receivesStreamWithMid", "[participant]") {
-
-    Session s{0, test::sfuConfig};
-    auto id = s.addParticipant("127.0.0.1", 23292);
-    auto participant = s.getParticipant(id);
-
-    SECTION("returns false for a non-existing stream") {
-        CHECK_FALSE(participant.sendsStreamWithMid("0"));
-    }
-
-    SECTION("returns true for an existing stream") {
-        SDP::SessionDescription sdp(test::sdpOfferLines1);
-        participant.addIncomingStream(Stream{sdp.mediaDescriptions[0]});
-        CHECK(participant.receivesStreamWithMid("0"));
-    }
-}
-*/
