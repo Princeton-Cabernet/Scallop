826bd0f964ec684903b8bb958daa407a6a775e76
Oliver Michel <omichel@princeton.edu>
2025-08-13 19:12:06 -0500

    add tofino agent

---
+++ README.md
@@ -0,0 +1,21 @@
+# Scallop SFU
+
+This project rethinks the key building block for video-conferencing infrastructures - selective forwarding units (SFUs). SFUs relay and adapt media streams between participants and, today, run in software on general-purpose servers. Our main insight, discerned from dissecting the operation of production SFU servers, is that SFUs largely mimic traditional packet-processing operations such as dropping and forwarding. Guided by this, we present **Scallop**, an SDN-inspired SFU that decouples video-conferencing applications into a hardware-based data plane for latency-sensitive and frequent media operations, and a software control plane for the (infrequent) remaining tasks, such as analyzing feedback signals and session management. Scallop is a general design that is suitable for a variety of hardware platforms, including programmable switches and SmartNICs. Our Tofino-based implementation fully supports WebRTC and delivers 7-422x improved scaling over a 32-core commodity server, while reaping performance improvements by cutting forwarding-induced latency by 26x.
+
+## Building the code
+
+### Controller and Switch Agent
+
+```shell
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc)
+```
+
+### Data Plane
+
+
+### Client Application
+
+
---
+++ tofino_agent/tofino_agent_main.cc
@@ -0,0 +1,104 @@
+
+#include <iostream>
+#include <cxxopts/cxxopts.h>
+
+#include "../lib/tofino_data_plane.h"
+#include "../lib/switch_agent.h"
+
+void printHelp(cxxopts::Options& opts, int exitCode = 0) {
+
+    std::ostream& os = (exitCode ? std::cerr : std::cout);
+    os << opts.help({""}) << std::endl;
+    exit(exitCode);
+}
+
+cxxopts::Options setOptions() {
+
+    cxxopts::Options opts("tofino_agent", "P4-SFU Tofino Agent");
+
+    opts.add_options()
+        ("i,switch-id", "switch id", cxxopts::value<unsigned>(), "ID")
+        ("l,data-plane-iface", "data-plane interface", cxxopts::value<std::string>(), "IFACE")
+        ("c,controller", "controller address", cxxopts::value<std::string>(),"IP:PORT")
+        ("u,ice-ufrag", "ICE user-name fragment", cxxopts::value<std::string>(), "UFRAG")
+        ("p,ice-pwd", "ICE password", cxxopts::value<std::string>(), "PWD")
+        ("x,api-listen-port", "API listen port", cxxopts::value<std::uint16_t>(), "PORT")
+        ("v,verbose", "log debug messages")
+        ("h,help", "print this help message");
+
+    return opts;
+}
+
+template <typename DataPlaneType>
+typename p4sfu::SwitchAgent<DataPlaneType>::Config parseOptions(cxxopts::Options opts, int argc, char** argv) {
+
+    typename p4sfu::SwitchAgent<DataPlaneType>::Config config{
+        .switchId       = 0,
+        .apiListenPort  = 4001,
+        .controllerIPv4 = "127.0.0.1",
+        .controllerPort = 3302,
+        .iceUfrag       = "7Ad/",
+        .icePwd         = "UKZe/aYNEouGzQUhChnKGiIS",
+        .verbose        = false
+    };
+
+    auto parsed = opts.parse(argc, argv);
+
+    if (parsed.count("i")) {
+        config.switchId = parsed["i"].as<unsigned>();
+    }
+
+    if (parsed.count("l")) {
+        config.dataPlaneIface = parsed["l"].as<std::string>();
+    } else {
+        printHelp(opts, 1);
+    }
+
+    if (parsed.count("c")) {
+        std::tie(config.controllerIPv4 ,config.controllerPort)
+                = util::parseIPPort(parsed["c"].as<std::string>());
+    }
+
+    if (parsed.count("u")) {
+        config.iceUfrag = parsed["u"].as<std::string>();
+    }
+
+    if (parsed.count("p")) {
+        config.icePwd = parsed["p"].as<std::string>();
+    }
+
+    if (parsed.count("x")) {
+        config.apiListenPort = parsed["x"].as<std::uint16_t>();
+    }
+
+    if (parsed.count("v")) {
+        config.verbose = true;
+    }
+
+    if (parsed.count("h")) {
+        printHelp(opts);
+    }
+
+    return config;
+}
+
+int main(int argc, char** argv) {
+
+    auto config = parseOptions<p4sfu::TofinoDataPlane>(setOptions(), argc, argv);
+
+    config.type = p4sfu::SwitchAgent<p4sfu::TofinoDataPlane>::Config::Type::tofino;
+
+    p4sfu::TofinoDataPlane::Config dataPlaneConfig{
+        .verbose = config.verbose,
+        .tofinoListenPort = 8765,
+        .dataPlaneIface = config.dataPlaneIface
+    };
+
+    try {
+        p4sfu::SwitchAgent<p4sfu::TofinoDataPlane> s(config, dataPlaneConfig);
+        return s();
+    } catch (std::exception& e) {
+        std::cerr << "failed initializing Tofino agent: " << e.what() << std::endl;
+        return -1;
+    }
+}
